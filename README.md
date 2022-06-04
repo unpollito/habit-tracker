0e268a26dc5520caad1c86d39190621142646bc0
David Martín-González <3705952+unpollito@users.noreply.github.com>
2022-06-04 19:07:29 +0100

    Better DB schema, add migration info on README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # habit-tracker
 Habit tracker app
+
+## Setup
+
+### Backend
+
+1. Make sure that you have set up your Rust toolchain correctly. Check out
+   the [instructions](https://rust-lang.github.io/rustup/installation/index.html)
+   in the official Rust documentation.
+
+2. Ensure that you have a [PostgreSQL](https://www.postgresql.org/) database
+   running.
+   
+3. Create a `backend/refinery.toml` file using `backend/refinery.toml.sample`
+   as a reference, with your DB access data.
+
+4. Install `refinery_cli`: `cargo install refinery_cli`.
+
+5. Set up DB by running migrations: `cd backend` and
+   `refinery migrate -c refinery.toml -p src/db/migrations`.
---
+++ backend/src/db/migrations/V1__initial_db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE public.user_account
+(
+    id         uuid,
+    username   character varying(20) NOT NULL,
+    password   character(96)         NOT NULL,
+    email      character varying(80) NOT NULL,
+    is_enabled boolean               NOT NULL DEFAULT FALSE,
+    created_at timestamp             NOT NULL DEFAULT NOW(),
+    updated_at timestamp             NOT NULL DEFAULT NOW(),
+    deleted_at timestamp             NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT user_account_unique_username UNIQUE (username),
+    CONSTRAINT user_account_unique_email UNIQUE (email)
+);
+
+CREATE TABLE public.user_activation_token
+(
+    user_id    uuid      NOT NULL,
+    token      character(172),
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES user_account (id) ON DELETE CASCADE
+)
